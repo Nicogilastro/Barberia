f7e7de1151577f8ec0a31d5f7e80f5637afd769e
Nicogilastro <75554628+Nicogilastro@users.noreply.github.com>
2022-08-25 19:01:50 -0300

    Update book.php

--- book.php
+++ book.php
@@ -5,8 +5,6 @@
   use Twilio\Rest\Client;
   
   require './phpmailer/vendor/autoload.php';
-  require './message/twillio/jwt/client';
-
 
   $conn = mysqli_connect('localhost', 'root', '', 'bd');
 
@@ -30,41 +28,6 @@
     readfile("./success/success.html");
 
     readfile("./message/1.bat");
-   
-
-
-  //   $mail = new PHPMailer;
-  //   //Enable SMTP debugging.
-  //   $mail->SMTPDebug = 0;                           
-  //   //Set PHPMailer to use SMTP.
-  //   $mail->isSMTP();        
-  //   //Set SMTP host name                      
-  //   $mail->Host = "smtp.gmail.com";
-  //   //Set this to true if SMTP host requires authentication to send email
-  //   $mail->SMTPAuth = TRUE;                      
-  //   //Provide username and password
-  //   $mail->Username = "";             
-  //   $mail->Password = "";                       
-  //   //If SMTP requires TLS encryption then set it
-  //   $mail->SMTPSecure = "tls";                       
-  //   //Set TCP port to connect to
-  //   $mail->Port = 587;                    
-  //   $mail->From = "";
-  //   $mail->FromName = "Full Name";
-  //   $mail->addAddress("1132551226@vtext.com", "Nico");
-  //   $mail->isHTML(true);
-  //   $mail->Subject = "Subject Text";
-  //   $mail->Body = "<i>Mail body in HTML</i>";
-  //   $mail->AltBody = "This is the plain text version of the email content";
-  //   if(!$mail->send()) {
-  //     echo "";
-  //   }
-  //   else{
-  //     echo "Message has been sent successfully";
-  //   }
-  // } else {
-  //   echo "error";
-  // }
 
   }
 
