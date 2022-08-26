a4c261635be8801e10e95ca5ca0a968fa87c3258
Nicogilastro <75554628+Nicogilastro@users.noreply.github.com>
2022-08-26 00:28:07 -0300

    Update book.php

--- book.php
+++ book.php
@@ -29,25 +29,19 @@
 
     // Required if your environment does not handle autoloading
     require __DIR__ . '/vendor/autoload.php';
-    
-    // Use the REST API Client to make requests to the Twilio REST API
-    
-    // Your Account SID and Auth Token from twilio.com/console
-    $sid = 'AC705c4a96adfbec3eeb75cc31caad3ff9';
-    $token = '3d449aed513c044bdb9f317dbf3a6c19';
-    $client = new Client($sid, $token);
-    
-    // Use the client to do fun stuff like send text messages!
-    $client->messages->create(
-        // the number you'd like to send the message to
-        '+541166577020',
-        [
-            // A Twilio phone number you purchased at twilio.com/console
-            'from' => '+541132551226',
-            // the body of the text message you'd like to send
-            'body' => 'que te pasa pedazo de puta pelotuda!'
-        ]
-    );
+    $sid    = ""; 
+    $token  = ""; 
+    $twilio = new Client($sid, $token); 
+     
+    $message = $twilio->messages 
+                      ->create("whatsapp:+5491132551226", // to 
+                        array( 
+                            "from" => "whatsapp:+14155238886",       
+                            "body" => "Hola, queria hacer una reserva. Soy " . "$Nombre" . " " . "$Apellido" . ", mi dni es " . "$Dni" . " y mi telefono es " . "$Telefono" . "." . " El dia seria, el " . "$Fecha" . " a las " . "$Horario" . ". Muchas Gracias!"
+                        ) 
+                      ); 
+     
+    print($message->sid);
   }
 
 ?>
